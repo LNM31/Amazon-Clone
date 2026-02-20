d6159a54b2597b2ec2b1e0351d67994fedd83642
LNM31 <licanemes31@gmail.com>
2026-02-20 15:10:23 +0200

    moved Amazon project to its own reposetory

---
+++ scripts/checkout.js
@@ -0,0 +1,26 @@
+import { renderOrderSummary } from "./checkout/orderSummary.js";
+import { renderPaymentSummary } from "./checkout/paymentSummary.js";
+import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
+import { loadProducts,loadProductsFetch } from "../data/products.js";
+import { loadCart,loadCartFetch } from "../data/cart.js";
+//import '../data/cart-class.js';
+//import '../data/backend-practice.js';
+
+async function loadPage()
+{
+  try {
+    //throw 'error1';
+    await Promise.all([
+      loadProductsFetch(),
+      loadCartFetch()
+    ]);
+
+  }catch (error) {
+    console.log('Unexpected error. Please try again later.');
+  }
+
+  renderCheckoutHeader();
+  renderOrderSummary();
+  renderPaymentSummary();
+}
+loadPage();
